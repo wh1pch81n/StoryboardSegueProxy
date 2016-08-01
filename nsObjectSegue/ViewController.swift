0b264d6d75f334844654c7951b8e4f94ca1b8dce
Derrick Ho <derrickho328@gmail.com>
2016-07-31 23:20:58 -0400

    initial commit

--- nsObjectSegue/ViewController.swift
+++ nsObjectSegue/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+	@IBOutlet var segueToViewController: StoryboardSegueProxy!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -20,6 +21,49 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func tapped(sender: AnyObject) {
+//		segueToViewController.presentModally()
+		segueToViewController.performSegue()
+	}
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		print("will segue to \(segue.destinationViewController)")
+	}
 
 }
 
+class StoryboardSegueProxy: NSObject {
+	@IBOutlet weak var presentingViewController: UIViewController!
+	@IBInspectable var storyboardName: String = "Main"
+	@IBInspectable var viewControllerStoryboardID: String = ""
+	@IBInspectable var bundleIdentifier: String = ""
+	@IBOutlet weak var button: UIButton! {
+		willSet {
+			newValue.addTarget(self, action: #selector(self.performSegue), forControlEvents: .TouchUpInside)
+		}
+	}
+	@IBOutlet weak var navigationController: UINavigationController! {
+		willSet {
+			let bundle = bundleIdentifier.isEmpty ? NSBundle.mainBundle() : NSBundle(identifier: bundleIdentifier)!
+			let storyboard = UIStoryboard(name: storyboardName, bundle: bundle)
+			let vc = storyboard.instantiateViewControllerWithIdentifier(viewControllerStoryboardID)
+			newValue.setViewControllers([vc], animated: false)
+		}
+	}
+	
+	func presentModally(sender: AnyObject? = nil) {
+		let bundle = bundleIdentifier.isEmpty ? NSBundle.mainBundle() : NSBundle(identifier: bundleIdentifier)!
+		let storyboard = UIStoryboard(name: storyboardName, bundle: bundle)
+		let vc = storyboard.instantiateViewControllerWithIdentifier(viewControllerStoryboardID)
+		presentingViewController.presentViewController(vc, animated: true, completion: nil)
+	}
+	
+	func performSegue() {
+		let bundle = bundleIdentifier.isEmpty ? NSBundle.mainBundle() : NSBundle(identifier: bundleIdentifier)!
+		let storyboard = UIStoryboard(name: storyboardName, bundle: bundle)
+		let vc = storyboard.instantiateViewControllerWithIdentifier(viewControllerStoryboardID)
+		let s = UIStoryboardSegue(identifier: nil, source: presentingViewController, destination: vc)
+		presentingViewController.prepareForSegue(s, sender: self)
+		presentingViewController.showViewController(vc, sender: presentingViewController)
+	}
+}
